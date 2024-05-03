3d0b731280bca7eb16cb8b9862f280f76e8f1e55
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-03 06:26:51 +0300

    Update download.py

--- ZeMusic/plugins/play/download.py
+++ ZeMusic/plugins/play/download.py
@@ -51,7 +51,7 @@ async def song_downloader(client, message: Message):
             info_dict = ydl.extract_info(link, download=False)
             audio_file = ydl.prepare_filename(info_dict)
             ydl.process_info(info_dict)
-        rep = f"""href="https://t.me/LL0_BOT?start=1111"> ➮ 𝐁𝐎𝐓 𝐋𝐎𝐋</a></b>""",
+        rep = f"""<b>  <b><a href="https://t.me/LL0_BOT?start=1111"> ➮ 𝐁𝐎𝐓 𝐋𝐎𝐋</a></b>""",
         host = str(info_dict["uploader"])
         secmul, dur, dur_arr = 1, 0, duration.split(":")
         for i in range(len(dur_arr) - 1, -1, -1):
