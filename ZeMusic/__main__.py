dea6ce28440c3fa0a8fd7c05b8d28c44a61797f4
fasdxoox <163231340+fasdxoox@users.noreply.github.com>
2024-05-15 02:46:49 +0300

    Update __main__.py

--- ZeMusic/__main__.py
+++ ZeMusic/__main__.py
@@ -50,7 +50,7 @@ async def init():
         pass
     await Mody.decorators()
     LOGGER("ZeMusic").info(
-        "جاري تشغيل البوت\nتم التنصيب على سورس لـول بنجاح\nقناة السورس https://t.me/EF_19"
+        "جاري تشغيل البوت\nتم التنصيب على سورس لـول بنجاح\nقناة السورس https://t.me/K55DD"
     )
     await idle()
     await app.stop()
